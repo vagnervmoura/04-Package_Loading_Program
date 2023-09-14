3beff4b7f9ff503f4b1258ec36da482d76bdc6c5
Vagner Vignoto Moura <eu@vagnermoura.com>
2023-09-14 18:33:48 +0100

    Finished project, all steps done.

--- main.py
+++ main.py
@@ -1,27 +1,61 @@
 # 04 - Package Loading Program
 
-list_n_items = []
 list_n_package = []
 package = 0
-item = 0
+n_package = 1
+item_w = 0
 n_item = 0
 min = 1
 max = 10
+package_kg_total = 0
+non_optimal_package = 20
+min_unused_n_package = None
+amount_unused_package = None
 
 n_itens_to_ship = int(input("Enter the number of items to ship: "))
+
 while n_itens_to_ship > n_item:
     try:
-        item_w = int(input("Enter the weight of item " + str(n_item+1) + ": "))
+        item_w = float(input("Enter the weight of item " + str(n_item+1) + ": "))
         if item_w == 0:
             break
         elif min <= item_w <= max:
             if package + item_w <= 20:
                 package = package + item_w
-                print("Package Weight: ", package)
+                print("Package {} is loaded with {}kg, total of {}kg is available to fill this package.\n\n".format(n_package, package, 20 - package))
                 n_item += 1
-            else:
-                print("Lets to pack next package")
+                list_n_package.append(["N_Package:", n_package, "Package Kilo:", package, "N_Item:", n_item, "Item Kilo:", item_w])
+                package_kg_total = package_kg_total + item_w
+            elif n_itens_to_ship > n_item:
+                print("Package {}, its in the maximun load with {}kg. \nCannot add this item with {}kg to this package, sending this Item to the next Package.\n\n".format(n_package, package, item_w))
+                if package < 20 and package < non_optimal_package:
+                    min_unused_n_package = n_package
+                    amount_unused_package = 20 - package
+                    non_optimal_package = package
+
+                package = 0
+                n_package += 1
+                n_item += 1
+                package = package + item_w
+                list_n_package.append(["N_Package:", n_package, "Package Kilo:", package, "N_Item:", n_item, "Item Kilo:", item_w])
+                package_kg_total = package_kg_total + item_w 
         elif item_w > max:
-            print("Item to heavier than 10kg, please remove:", item_w - 10,"from your Item.")
+            print("Item to heavier than 10kg, please remove: {}kg from your Item.".format(item_w - 10))
     except ValueError:
         print("Sorry, but you have input a wrong weight, please let's try again with an valid weight.")
+
+print("=" *100)
+
+print(n_package, "Packages to send.")
+print(package_kg_total, "Kilos total to send.") 
+print(n_package * 20 - package_kg_total, "Total unused of capacity due a non-optimal packaging.")
+
+if package < 20 and package < non_optimal_package:
+    min_unused_n_package = n_package
+    amount_unused_package = 20 - package
+    non_optimal_package = package
+    print("Package Number: {} had the most 'unused' capacity, with an amount of {}kg unused.".format(min_unused_n_package, amount_unused_package))
+else:
+    print("Package Number: {} had the most 'unused' capacity, with an amount of {}kg unused.".format(min_unused_n_package, amount_unused_package))
+                  
+print("=" *100)  
