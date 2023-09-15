57b48ec611a80b26d621f683c582c05f838150fb
Vagner Vignoto Moura <eu@vagnermoura.com>
2023-09-15 20:54:41 +0100

    Changed code if INPUT 0 on the first ITEM, exit program.

--- main.py
+++ main.py
@@ -12,13 +12,23 @@ non_optimal_package = 20
 min_unused_n_package = None
 amount_unused_package = None
 
+def goto(linenum):
+    global line
+    line = linenum
+
+line = 1
+
 n_itens_to_ship = int(input("Enter the number of items to ship: "))
 
 while n_itens_to_ship > n_item:
     try:
         item_w = float(input("Enter the weight of item " + str(n_item+1) + ": "))
         if item_w == 0:
-            break
+            if n_package == 1 and package == 0:
+                print("We do not have any Package to send.\n\n")
+                exit()
+            else:
+                break
         elif min <= item_w <= max:
             if package + item_w <= 20:
                 package = package + item_w
@@ -27,7 +37,7 @@ while n_itens_to_ship > n_item:
                 list_n_package.append(["N_Package:", n_package, "Package Kilo:", package, "N_Item:", n_item, "Item Kilo:", item_w])
                 package_kg_total = package_kg_total + item_w
             elif n_itens_to_ship > n_item:
-                print("Package {}, its in the maximun load with {}kg. \nCannot add this item with {}kg to this package, sending this Item to the next Package.\n\n".format(n_package, package, item_w))
+                print("Package {}, its in the maximun load. Cannot add this item with {}kg to this package, sending this Item to the next Package.".format(n_package, item_w))
                 if package < 20 and package < non_optimal_package:
                     min_unused_n_package = n_package
                     amount_unused_package = 20 - package
@@ -43,7 +53,7 @@ while n_itens_to_ship > n_item:
             print("Item to heavier than 10kg, please remove: {}kg from your Item.".format(item_w - 10))
     except ValueError:
         print("Sorry, but you have input a wrong weight, please let's try again with an valid weight.")
-
+    line == 100
 print("=" *100)
 
 print(n_package, "Packages to send.")
@@ -54,8 +64,8 @@ if package < 20 and package < non_optimal_package:
     min_unused_n_package = n_package
     amount_unused_package = 20 - package
     non_optimal_package = package
-    print("Package Number: {} had the most 'unused' capacity, with an amount of {}kg unused.".format(min_unused_n_package, amount_unused_package))
+    print("Package Number:", min_unused_n_package, "had the most 'unused' capacity, with an amount of", amount_unused_package, "kg unused.")
 else:
-    print("Package Number: {} had the most 'unused' capacity, with an amount of {}kg unused.".format(min_unused_n_package, amount_unused_package))
+    print("Package Number:", min_unused_n_package, "had the most 'unused' capacity, with an amount of", amount_unused_package, "kg unused.")
                   
 print("=" *100)  
